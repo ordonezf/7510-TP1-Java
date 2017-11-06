d91f054e5b5c64d1a82e95ebb7ce3a478b4e0ca2
fran <ordonez.f93@gmail.com>
2017-11-06 18:13:23 -0300

    Add InvalidDatabaseException

---
+++ src/main/java/ar/uba/fi/tdd/rulogic/model/Exceptions/InvalidDatabaseException.java
@@ -0,0 +1,8 @@
+package ar.uba.fi.tdd.rulogic.model.Exceptions;
+
+public class InvalidDatabaseException extends Exception {
+
+    public InvalidDatabaseException(String msg){
+        super(msg);
+    }
+}
