ab9e84056510483c424de8131883d111d07ab1c1
fran <ordonez.f93@gmail.com>
2017-11-06 18:12:38 -0300

    Remove unused imports

--- src/main/java/ar/uba/fi/tdd/rulogic/model/Entities/Rule.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/model/Entities/Rule.java
@@ -1,25 +1,39 @@
 package ar.uba.fi.tdd.rulogic.model.Entities;
 
-import ar.uba.fi.tdd.rulogic.model.Entities.Fact;
-
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
+import java.util.Map;
 
 public class Rule {
 
     private String name;
     private List<String> args;
-    private List<Fact> conditions;
+    private List<Fact> facts;
 
     public Rule(String name, List<String> args, List<Fact> conditions) {
         this.name = name;
         this.args = args;
-        this.conditions = conditions;
+        this.facts = conditions;
 
     }
 
     public boolean isEqual(Question q) {
         return this.name.equals(q.getName()) && this.args.size() == q.getArgs().size();
+    }
+
+    public List<String> getArgs() {
+        return args;
+    }
+
+    public List<Fact> transformFacts(Map<String, String> merge) {
+        List<Fact> f = new ArrayList<Fact>();
+        for (Fact fact : this.facts) {
+            List<String> l = new ArrayList<String>();
+            for (String s : fact.getArgs()) {
+                l.add(merge.get(s));
+            }
+            f.add(new Fact(fact.getName(), l));
+        }
+        return f;
     }
 }
