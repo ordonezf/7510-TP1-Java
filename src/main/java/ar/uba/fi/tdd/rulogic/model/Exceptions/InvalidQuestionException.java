f33bda24fc671d2acdc66a98da975c0276421b62
fran <ordonez.f93@gmail.com>
2017-11-06 19:37:04 -0300

    Add InvalidQuestionException

---
+++ src/main/java/ar/uba/fi/tdd/rulogic/model/Exceptions/InvalidQuestionException.java
@@ -0,0 +1,7 @@
+package ar.uba.fi.tdd.rulogic.model.Exceptions;
+
+public class InvalidQuestionException extends Exception {
+    public InvalidQuestionException(String msg){
+        super(msg);
+    }
+}
